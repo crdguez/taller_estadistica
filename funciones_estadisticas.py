83907eeb68ef7be02f43494f5c2bbc8eabece74f
crdguez <carlos.rodriguez.jaso@gmail.com>
2022-04-19 07:50:34 +0200

    z

--- funciones_estadisticas.py
+++ funciones_estadisticas.py
@@ -1,19 +1,94 @@
-# from sympy import *
+from sympy import *
 import pandas as pd
 import numpy as np
 # import scipy as sc
 import matplotlib.pyplot as plt
 
+def agrupar_por_intervalos(datos, n) :
+    # agrupa los datos en n Intervalos
+    rango=(5*int(datos.min()/5),5*(int(datos.max()/5)+1))
+    frecs, intervalos= np.histogram(datos, range=rango, bins=n)
+    intervalos_latex="$"+r", ".join([latex(Interval.Ropen(intervalos[i],intervalos[i+1])) for i in range(len(intervalos)-1)])+"$"
+    return frecs, intervalos, intervalos_latex
+
+def percentil(tabla, k=50) :
+    # percentil si cae en medio de dos valores, devuelve el punto medio
+    t = tabla.where(tabla['F_i']/tabla['f_i'].sum()>=k/100).dropna()
+    if t.iloc[0]['F_i']/tabla['f_i'].sum() == k/100 :
+        return (t.iloc[0]['x_i']+t.iloc[1]['x_i'])/2
+    else :
+        return t.iloc[0]['x_i']
+
+    # tabla.where(tabla['F_i']/tabla['f_i'].sum()>=k/100).dropna().iloc[0]['x_i']
+    # return tabla.where(tabla['F_i']/tabla['f_i'].sum()>k/100).dropna().iloc[0]['x_i']
+
+def analisis_agrupado(frecs=[], intervalos=[]):
+    #x_i, f_i, F_i, r_i  = np.unique(datos),np.bincount(datos), cumfreq(datos, numbins=len(np.unique(datos)))[0].astype(int), np.multiply(relfreq(datos, numbins=len(np.unique(datos)))[0],100)
+
+    # Tabla de Frecuencias
+
+    tabla = pd.DataFrame({'L_i':intervalos[:-1], 'R_i':intervalos[1:] ,
+                        'x_i': (intervalos[:-1]+intervalos[1:])/2,
+                        'f_i':frecs,
+                        'F_i':np.cumsum(frecs),
+                        'h_i':frecs/np.sum(frecs),
+                        # 'H_i':(np.unique(datos, return_counts=True)[1]/len(datos)).cumsum(),
+    #                       # '%_i':np.unique(datos, return_counts=True)[1]*100/len(datos),
+    #                       # '%A_i':(np.unique(datos, return_counts=True)[1]*100/len(datos)).cumsum(),
+    #                       # 'xf':np.unique(datos, return_counts=True)[0]*np.unique(datos, return_counts=True)[1],
+    #                       # 'x2f':np.unique(datos, return_counts=True)[0]**2*np.unique(datos, return_counts=True)[1],
+                         }
+                         )
+                        # ).set_index('x_i')
+    # tabla.reset_index(inplace=True)
+    # tabla.loc['suma']=tabla.sum()
+    # d = np.diff(np.unique(datos)).min()
+    # left_of_first_bin = datos.min() - float(d)/2
+    # right_of_last_bin = datos.max() + float(d)/2
+    # fg, ax = plt.subplots()
+    # plt.clf()
+    # plt.hist(datos, np.arange(left_of_first_bin, right_of_last_bin + d, d), rwidth=0.9, cumulative = False)
+    # plt.title("Diagrama de barras")
+
+    dic=dict()
+    dic['frecs']=frecs
+    dic['intervalos']=intervalos
+    dic['intervalos_latex']="$"+r", ".join([latex(Interval.Ropen(intervalos[i],intervalos[i+1])) for i in range(len(intervalos)-1)])+"$"
+    # dic['datos']=datos
+    dic['tabla']=tabla
+    # dic['media']=datos.mean()
+    # dic['figure']=fg
+    # dic['moda']= tabla[:-1].iloc[tabla[:-1]['f_i'].idxmax(),:]['x_i']
+    # dic['mediana']=percentil(tabla[:-1],50)
+    # dic['rango']=tabla[:-1]['x_i'].max()-tabla[:-1]['x_i'].min()
+    # dic['varianza']=(tabla[-1:]['x2f'][0]/tabla[-1:]['f_i'][0]-dic['media']**2)
+    # dic['desviacion']=sqrt(dic['varianza'])
+    # dic['texto_media'] = r' $\overline{x}=\dfrac{\Sigma{x_i f_i}}{N}=' \
+    #     +r'\dfrac{'+ str(tabla.iloc[:-1,7].sum()) \
+    #     +r'}{'+str(tabla.iloc[:-1,1].sum())+r'}='+ \
+    #     str(tabla.iloc[:-1,7].sum()/tabla.iloc[:-1,1].sum())+r'$'
+    # dic['texto_moda']=r' $Mo='+ \
+    #     str(tabla[:-1].iloc[tabla[:-1]['f_i'].idxmax(),:]['x_i'])+r'$'
+    # dic['texto_mediana']=r' $Me='+ \
+    #     str(dic['mediana'])+r'$'
+    # dic['texto_desviacion']=r'$\sigma=\sqrt{\dfrac{\Sigma{x_i^2 f_i}}{N}-\overline{x}^2}=' \
+    #     +r'\sqrt{\dfrac{'+str(tabla[-1:]['x2f'][0])+r'}{'+str(tabla.iloc[:-1,1].sum())+'}-' \
+    #     +str(dic['media'])+'^2}'+'=' \
+    #     +str(sqrt((tabla[-1:]['x2f'][0]/tabla[-1:]['f_i'][0]-dic['media']**2)))+r'$'
+    return  dic
 
 def analisis_discreto(str_datos):
     datos = np.loadtxt(str_datos.split())
     datos = datos.astype(int)
     #x_i, f_i, F_i, r_i  = np.unique(datos),np.bincount(datos), cumfreq(datos, numbins=len(np.unique(datos)))[0].astype(int), np.multiply(relfreq(datos, numbins=len(np.unique(datos)))[0],100)
-    tabla = pd.DataFrame({'x_i':np.unique(datos), 'f_i':np.unique(datos, return_counts=True)[1], 
-                          'F_i':np.unique(datos, return_counts=True)[1].cumsum(), 
-                          'h_i':np.unique(datos, return_counts=True)[1]/len(datos), 
-                          'H_i':(np.unique(datos, return_counts=True)[1]/len(datos)).cumsum(), 
-                          '%_i':np.unique(datos, return_counts=True)[1]*100/len(datos), 
+
+    # Tabla de Frecuencias
+
+    tabla = pd.DataFrame({'x_i':np.unique(datos), 'f_i':np.unique(datos, return_counts=True)[1],
+                          'F_i':np.unique(datos, return_counts=True)[1].cumsum(),
+                          'h_i':np.unique(datos, return_counts=True)[1]/len(datos),
+                          'H_i':(np.unique(datos, return_counts=True)[1]/len(datos)).cumsum(),
+                          '%_i':np.unique(datos, return_counts=True)[1]*100/len(datos),
                           '%A_i':(np.unique(datos, return_counts=True)[1]*100/len(datos)).cumsum(),
                           'xf':np.unique(datos, return_counts=True)[0]*np.unique(datos, return_counts=True)[1],
                           'x2f':np.unique(datos, return_counts=True)[0]**2*np.unique(datos, return_counts=True)[1],
@@ -29,12 +104,26 @@ def analisis_discreto(str_datos):
     plt.hist(datos, np.arange(left_of_first_bin, right_of_last_bin + d, d), rwidth=0.9, cumulative = False)
     plt.title("Diagrama de barras")
 
-#     solucion.append({"media":datos.mean(), "mediana":np.percentile(datos,50), "moda":stats.mode(datos)})
-#     solucion.append({"P70":np.percentile(datos,70), "Q1":np.percentile(datos,25),"Q3":np.percentile(datos,75),"D4":np.percentile(datos,40),})
-#     solucion.append({"rango":np.amax(datos)-np.amin(datos), "varianza": np.var(datos), "desviación típica":sqrt(np.var(datos)), "coeficiente variación": sqrt(np.var(datos))/abs(np.mean(datos))})
-#     display(tabla, solucion[2] , solucion[3], solucion[4])
     dic=dict()
     dic['datos']=datos
     dic['tabla']=tabla
     dic['figure']=fg
+    dic['media']=datos.mean()
+    dic['moda']= tabla[:-1].iloc[tabla[:-1]['f_i'].idxmax(),:]['x_i']
+    dic['mediana']=percentil(tabla[:-1],50)
+    dic['rango']=tabla[:-1]['x_i'].max()-tabla[:-1]['x_i'].min()
+    dic['varianza']=(tabla[-1:]['x2f'][0]/tabla[-1:]['f_i'][0]-dic['media']**2)
+    dic['desviacion']=sqrt(dic['varianza'])
+    dic['texto_media'] = r' $\overline{x}=\dfrac{\Sigma{x_i f_i}}{N}=' \
+        +r'\dfrac{'+ str(tabla.iloc[:-1,7].sum()) \
+        +r'}{'+str(tabla.iloc[:-1,1].sum())+r'}='+ \
+        str(tabla.iloc[:-1,7].sum()/tabla.iloc[:-1,1].sum())+r'$'
+    dic['texto_moda']=r' $Mo='+ \
+        str(tabla[:-1].iloc[tabla[:-1]['f_i'].idxmax(),:]['x_i'])+r'$'
+    dic['texto_mediana']=r' $Me='+ \
+        str(dic['mediana'])+r'$'
+    dic['texto_desviacion']=r'$\sigma=\sqrt{\dfrac{\Sigma{x_i^2 f_i}}{N}-\overline{x}^2}=' \
+        +r'\sqrt{\dfrac{'+str(tabla[-1:]['x2f'][0])+r'}{'+str(tabla.iloc[:-1,1].sum())+'}-' \
+        +str(dic['media'])+'^2}'+'=' \
+        +str(sqrt((tabla[-1:]['x2f'][0]/tabla[-1:]['f_i'][0]-dic['media']**2)))+r'$'
     return  dic
--- main.py
+++ main.py
@@ -43,29 +43,148 @@ def unidimensional() :
         st.write(", ".join(map(str,dic['datos'])))
 
         # st.write(dic)
-        st.subheader("Tabla de Frecuencias:")
-        dic['tabla'].index=dic['tabla'].index.astype(str)
-        st.table(dic['tabla'].astype({"x_i":int,
-            "f_i":int,"F_i":int}).style.format({'h_i':"{:,.2f}",
-            'H_i':"{:,.2f}",'%_i':"{:,.2f}%",
-            '%A_i':"{:,.2f}%",'xf':"{:,.2f}",
-            'x2f':"{:,.2f}"}))
-        st.pyplot(dic['figure'])
-        st.subheader('Parámetros de centralización:')
-        tabla=dic['tabla']
-        media=r'$\overline{x}=\dfrac{\Sigma{x_i f_i}}{N}=' \
-            +r'\dfrac{'+ str(tabla.iloc[:-1,7].sum()) \
-            +r'}{'+str(tabla.iloc[:-1,1].sum())+r'}='+ \
-            str(tabla.iloc[:-1,7].sum()/tabla.iloc[:-1,1].sum())+r'$'
-        st.markdown('* Media: {} \
-            \n * Moda:{}'.format(media, dic['datos'].mean()))
+        col1, col2 = st.columns(2)
 
+        with col1 :
+            st.subheader("Tabla de Frecuencias:")
+            dic['tabla'].index=dic['tabla'].index.astype(str)
+            tabla_formateada=dic['tabla'].astype({"x_i":int,
+                "f_i":int,"F_i":int}).style.format({'h_i':"{:,.2f}",
+                'H_i':"{:,.2f}",'%_i':"{:,.2f}%",
+                '%A_i':"{:,.2f}%",'xf':"{:,.2f}",
+                'x2f':"{:,.2f}"})
+            # tabla_formateada=dic['tabla']
+            # tabla_formateada.rename(columns={'x_i':r'$x^2$'}, inplace = True)
+            st.table(tabla_formateada)
+            # st.dataframe(tabla_formateada)
+            # st.latex(tabla_formateada.to_latex())
+        with col2 :
+            st.pyplot(dic['figure'])
+
+        st.subheader('Parámetros de centralización:')
+        # tabla=dic['tabla']
+        # media=r'$\overline{x}=\dfrac{\Sigma{x_i f_i}}{N}=' \
+        #     +r'\dfrac{'+ str(tabla.iloc[:-1,7].sum()) \
+        #     +r'}{'+str(tabla.iloc[:-1,1].sum())+r'}='+ \
+        #     str(tabla.iloc[:-1,7].sum()/tabla.iloc[:-1,1].sum())+r'$'
+        st.markdown('* Media: {} \
+            \n * Moda:{} \
+            \n * Mediana: {} \
+            '.format(dic['texto_media'], dic['texto_moda'], \
+            dic['texto_mediana'],))
+        st.subheader('Párametros de posición')
+        # st.markdown('**Percentiles**')
+        k=st.slider('Selecciona percentil:',0,100,value=50,step=5)
+        st.write('Percentil $P_{'+str(k)+'}$= '+str(percentil(dic['tabla'][:-1],k)))
+        kk=st.radio('Selecciona cuartil: ',(25,50,75),format_func= lambda x: 'Q'+str(int(x/25)))
+        st.write('Cuartil Q'+str(int(kk/25))+'='+str(percentil(dic['tabla'][:-1],kk)))
+        st.subheader('Párametros de dispersión')
+        st.write('Rango: '+str(dic['rango']))
+        # st.write('Desviación: '+str(dic['desviacion']))
+        st.write('Desviación típica:  '+dic['texto_desviacion'])
+        st.write('Varianza:  $Var='+str(dic['desviacion'])+'^2='+str(dic['varianza'])+'$')
 
 
 
 def agrupada() :
+    st_datos=''
+    n_intervalos=0
     st.title("Estadística agrupada")
-    st.write("Aquí irá la estadística unidimensional agrupada")
+    st.sidebar.markdown("""---""")
+    st.warning(':fire: En construccion :fire:')
+    st.header("Datos de análisis:")
+    opciones = st.selectbox("Selecciona:", ('ejemplo1','entrada manual', 'entrada manual por intervalos'),index=0)
+    if opciones == 'ejemplo1' :
+        st_datos = """174 157 168 166 169 168 173 184 176 171 172 168 167 162 162 163
+                166 166 167 167 174 159 170 172 173 164 161 163 176 177"""
+        datos = np.loadtxt(st_datos.split())
+        st.write('Datos:')
+        st.write(', '.join(map(str,datos)))
+        # rango=(155,185)
+        n_intervalos=st.slider('Número de intervalos',2,10,value=3,step=1)
+        frecs, intervalos, intervalos_latex = agrupar_por_intervalos(datos,n_intervalos)
+
+    elif opciones == 'entrada manual' :
+        st_datos = st.text_input('Introduce los datos separados por espacios', '')
+        st_datos = st_datos.replace(',',' ')
+        if st_datos != '' :
+            datos = np.loadtxt(st_datos.split())
+            n_intervalos=st.slider('Número de intervalos',2,10,value=3,step=1)
+            frecs, intervalos, intervalos_latex = agrupar_por_intervalos(datos,n_intervalos)
+    else:
+        st_intervalos = st.text_input('Introduce la lista de límites de los intervalos:', '')
+        st_intervalos = st_intervalos.replace(',',' ')
+        intervalos = np.loadtxt(st_intervalos.split())
+        intervalos_latex="$"+r", ".join([latex(Interval.Ropen(intervalos[i],intervalos[i+1])) for i in range(len(intervalos)-1)])+"$"
+        n_intervalos=len(intervalos)-1
+        # st.write(n_intervalos)
+        st.write(intervalos_latex)
+        if st_intervalos != '' :
+            st_frecs = st.text_input('Introduce las frecuencias:', '')
+            st_frecs = st_frecs.replace(',',' ')
+            frecs=np.loadtxt(st_frecs.split())
+            if len(intervalos) != len(frecs) + 1 :
+                st.write("Reecuerda que tiene que haber un dato menos que la lista de intervalos")
+            else :
+                st.write(", ".join(map(str,frecs)))
+
+
+
+    # if st_datos != '' :
+    if n_intervalos != 0 :
+        dic=analisis_agrupado(frecs, intervalos)
+        st.write("**Datos agrupados:**")
+        st.write(dic)
+        st.write(np.cumsum(dic['frecs']))
+        st.write((intervalos[:-1]+intervalos[1:])/2)
+        # st.write(", ".join(map(str,datos)))
+        # st.write("**Intervalos:**")
+        # st.write(intervalos_latex)
+        #
+        # # st.write(dic)
+        col1, col2 = st.columns(2)
+
+        with col1 :
+            st.subheader("Tabla de Frecuencias:")
+            dic['tabla'].index=dic['tabla'].index.astype(str)
+        #     tabla_formateada=dic['tabla'].astype({"x_i":int,
+        #         "f_i":int,"F_i":int}).style.format({'h_i':"{:,.2f}",
+        #         'H_i':"{:,.2f}",'%_i':"{:,.2f}%",
+        #         '%A_i':"{:,.2f}%",'xf':"{:,.2f}",
+        #         'x2f':"{:,.2f}"})
+            # tabla_formateada=dic['tabla']
+        #     # tabla_formateada.rename(columns={'x_i':r'$x^2$'}, inplace = True)
+        #     st.table(tabla_formateada)
+            st.table(dic['tabla'])
+
+        #     # st.dataframe(tabla_formateada)
+        #     # st.latex(tabla_formateada.to_latex())
+        with col2 :
+            st.write('Aquí irá el Diagrama')
+        #     st.pyplot(dic['figure'])
+        #
+        st.subheader('Parámetros de centralización:')
+        # # tabla=dic['tabla']
+        # # media=r'$\overline{x}=\dfrac{\Sigma{x_i f_i}}{N}=' \
+        # #     +r'\dfrac{'+ str(tabla.iloc[:-1,7].sum()) \
+        # #     +r'}{'+str(tabla.iloc[:-1,1].sum())+r'}='+ \
+        # #     str(tabla.iloc[:-1,7].sum()/tabla.iloc[:-1,1].sum())+r'$'
+        # st.markdown('* Media: {} \
+        #     \n * Moda:{} \
+        #     \n * Mediana: {} \
+        #     '.format(dic['texto_media'], dic['texto_moda'], \
+        #     dic['texto_mediana'],))
+        # st.subheader('Párametros de posición')
+        # # st.markdown('**Percentiles**')
+        # k=st.slider('Selecciona percentil:',0,100,value=50,step=5)
+        # st.write('Percentil $P_{'+str(k)+'}$= '+str(percentil(dic['tabla'][:-1],k)))
+        # kk=st.radio('Selecciona cuartil: ',(25,50,75),format_func= lambda x: 'Q'+str(int(x/25)))
+        # st.write('Cuartil Q'+str(int(kk/25))+'='+str(percentil(dic['tabla'][:-1],kk)))
+        # st.subheader('Párametros de dispersión')
+        # st.write('Rango: '+str(dic['rango']))
+        # # st.write('Desviación: '+str(dic['desviacion']))
+        # st.write('Desviación típica:  '+dic['texto_desviacion'])
+        # st.write('Varianza:  $Var='+str(dic['desviacion'])+'^2='+str(dic['varianza'])+'$')
 
 def bidimensional() :
     st.title("Estadística bidimensinal y regresión lineal")
