66195ba58dc1336238a4875605096829bb3d746d
crdguez <carlos.rodriguez.jaso@gmail.com>
2025-05-28 19:52:20 +0200

    z

--- funciones_estadisticas.py
+++ funciones_estadisticas.py
@@ -170,7 +170,7 @@ def  analisis_bidimensional(datos, var1='x', var2='y'):
     numero_datos=tabla.shape[0]
 
     tabla2 =tabla
-    tabla2=tabla2.join(pd.DataFrame({'$'+var1+r'\cdot '+var2+r'$':datos[:,0]*datos[:,1],r'$'+var1+r'^2$':datos[:,0]**2, r'$'+var2+r'^2$':datos[:,1]**2}))
+    tabla2=tabla2.join(pd.DataFrame({'$'+var1+r'\cdot '+var2+r'$':datos[:,0]*datos[:,1],r'$'+var1+r'^2$':datos[:,0]**2, r'$'+var2+r'^2$':datos[:,1]**2})).applymap(lambda x: int(x) if x == int(x) else round(x, 2))
     tabla2.loc['Sumas']=tabla2.sum()
     tabla2.loc['Medias']=tabla2.iloc[:-1].mean()
 
@@ -236,7 +236,7 @@ def  analisis_bidimensional(datos, var1='x', var2='y'):
     # dic['tabla_ini_latex']=tabulate(tabla, headers="keys", tablefmt="latex",showindex = False).replace('\\$','$').replace('textbackslash{}','')
     # dic['tabla_fin_latex']=tabulate(tabla2, headers="keys", tablefmt="latex",showindex = True).replace('\\$','$').replace('textbackslash{}','').replace('\^{}','^')
     tabla2.index = tabla2.index.map(str)
-    dic['tabla_fin']=tabla2.applymap(lambda x: int(x) if x == int(x) else round(x, 2)).style.highlight_max(axis=0)
+    dic['tabla_fin']=tabla2.style.highlight_max(axis=0)
     dic['latex_medias']=latex_medias
     dic['latex_centro']=latex_centro
     dic['latex_varianzas']=latex_varianzas
